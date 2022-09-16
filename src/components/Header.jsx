ce31379c9e678ec1a7f94cfd8daa6213f99f471e
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-09-16 16:55:33 +0300

    header was added

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src= "../images/troll-face.png" alt="troll face"  className="header--image"/>
+      <h2 className="header--title">Meme Generator</h2>
+    </header>
+  );
+}
+
+export default Header;
